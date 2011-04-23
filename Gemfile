2f3fa5739bde3fa4036e5d100c8e82dd4212b388
Chia-Ling Lee <spiderman0103@gmail.com>
2011-04-23 09:05:56 +0800

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 group :test do
 	gem 'rspec', '2.5.0'
 	gem 'webrat', '0.7.1'
+	gem 'spork', '0.9.0.rc5'
 end
 
 gem 'rails', '3.0.7'
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,46 @@
 require 'spec_helper'
 
 describe PagesController do
-
-  describe "GET 'home'" do
+  render_views
+  
+  describe "GET 'home'" do #human read
     it "should be successful" do
-      get 'home'
+      get 'home'  #actually do
       response.should be_success
     end
   end
-
+  
+  it "should have the right title" do
+    get 'home'
+    response.should have_selector("title", 
+                      :content => "Ruby on Rails Tutorial Sample App | Home")
+  end
+  
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
   end
-
+  
+  it "should have the right title" do
+    get 'contact'
+    response.should have_selector("title", 
+                      :content => "Ruby on Rails Tutorial Sample App | Contact")
+  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+  
+  it "should have the right title" do
+    get 'about'
+    response.should have_selector("title", 
+                      :content => "Ruby on Rails Tutorial Sample App | About")
+  end
+  
+  
 end
