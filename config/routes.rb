7231ec59c9e3ec1c806106771ead462e3bcc1276
Chia-Ling Lee <spiderman0103@gmail.com>
2011-05-13 17:58:21 +0800

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  
+  match '/signup',  :to => 'users#new'
+  
   #match '/', :to => 'pages#home'
   #resources :pages
   # The priority is based upon order of creation:
